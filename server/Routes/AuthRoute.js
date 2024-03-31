9026515c8e5346bde410f3220dae991f109d05b9
Ankit <agdjp56@gmail.com>
2024-03-31 12:45:46 +0530

    updated

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const router = express.Router();
-const { Signup, Login, createPost, getPostById, likePost, addComment, getAllPosts, updateAvatar } = require('../Controllers/AuthController');
+const express = require("express");
+const prouter = express.Router();
+const {
+  Signup,
+  Login,
+  createPost,
+  getPostById,
+  likePost,
+  addComment,
+  getAllPosts,
+  updateAvatar,
+} = require("../Controllers/AuthController");
 const { userVerification } = require("../Middlewares/AuthMiddleware");
-const { getUserProfile } = require('../Controllers/AuthController');
-const upload = require('../upload'); 
+const { getUserProfile } = require("../Controllers/AuthController");
+const upload = require("../upload");
 
-
-router.post('/signup', Signup);
-router.post('/login', Login);
-router.get('/profile', userVerification, getUserProfile);
-router.post('/blog', userVerification, upload.single('image'), createPost); // New route for creating posts.
-router.get('/posts/:postId', getPostById); 
+router.post("/signup", Signup);
+router.post("/login", Login);
+router.get("/profile", userVerification, getUserProfile);
+router.post("/blog", userVerification, upload.single("image"), createPost); // New route for creating posts.
+router.get("/posts/:postId", getPostById);
 
 // To like a post
-router.put('/posts/:id/like', userVerification, likePost);
+router.put("/posts/:id/like", userVerification, likePost);
 
 // To comment on a post
-router.post('/posts/:id/comment', userVerification, addComment);
+router.post("/posts/:id/comment", userVerification, addComment);
 
-router.put('/avatar', userVerification, updateAvatar);
+router.put("/avatar", userVerification, updateAvatar);
 
-
-router.get('/posts', getAllPosts);
-
-
-
-
+router.get("/posts", getAllPosts);
 
 module.exports = router;
