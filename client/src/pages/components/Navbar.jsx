c66afa44b8221f40a48c2d04cc372ad895b44250
Ankit <agdjp56@gmail.com>
2024-04-01 19:23:28 +0530

    hook updated

--- client/src/pages/components/Navbar.jsx
+++ client/src/pages/components/Navbar.jsx
@@ -1,33 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate, Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
-
-
-const Navbar = ({isAuthenticated}) => {
+const Navbar = ({ isAuthenticated }) => {
   const navigate = useNavigate();
-  const [cookies, setCookie ,removeCookie] = useCookies(["token"]);
-  const [isAuth, setAuth] = useState(false)
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
+  const [isAuth, setAuth] = useState(false);
   axios.defaults.withCredentials = true;
 
   useEffect(() => {
     const verifyCookie = async () => {
-      console.log(cookies.token)
+      console.log(cookies.token);
       if (!cookies.token) {
-        {}
+        {
+        }
       } else {
         try {
-          axios.defaults.withCredentials = true
-          const { data } = await axios.get(import.meta.env.VITE_APP_BACKEND +  "/profile", {
+          axios.defaults.withCredentials = true;
+          const { data } = await axios.get(
+            import.meta.env.VITE_APP_BACKEND + "/profile",
+            {
               headers: {
-                'Access-Control-Allow-Origin': 'https://blog-website-backend-gc3f.onrender.com', 
-                'Content-Type': 'application/json'
-            },
-            withCredentials:true
-          });
+                "Access-Control-Allow-Origin":
+                  "https://blog-website-backend-gc3f.onrender.com",
+                "Content-Type": "application/json",
+              },
+              withCredentials: true,
+            }
+          );
           if (data.user) {
-            setAuth(true);}
+            setAuth(true);
+          }
         } catch (error) {
           console.error("Verification failed", error);
         }
@@ -36,12 +40,9 @@ const Navbar = ({isAuthenticated}) => {
     verifyCookie();
   }, [cookies, navigate, removeCookie]);
 
-
-
-
   const Logout = () => {
     // removeCookie("token", { path: "/" });
-    setCookie("token", null)
+    setCookie("token", null);
     navigate("/login");
   };
 
@@ -67,60 +68,61 @@ const Navbar = ({isAuthenticated}) => {
             className="collapse navbar-collapse justify-content-end"
             id="navbarNav"
           >
-          {(!isAuth) ?
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/login"
-                >
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <button
-                  type="button"
-                  className="get-started btn btn-outline-dark"
-                  onClick={() => {navigate("/read")}}
-                >
-                  Read Blogs
-                </button>
-              </li>
-            </ul>
-            : 
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link
-                  className="nav-link active"
-                  aria-current="page"
-                  to="/create"
-                >
-                  Write
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/profile">
-                  Profile
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/read">
-                  Read
-                </Link>
-              </li>
-              <li className="nav-item">
-                <button
-                  type="button"
-                  className="get-started btn btn-outline-dark"
-                  onClick={Logout}
-                >
-                  Logout
-                </button>
-              </li>
-            </ul>
-
-          }
+            {!isAuth ? (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <button
+                    className="nav-link active"
+                    aria-current="page"
+                    onClick={() => navigate("/login")}
+                  >
+                    Login
+                  </button>
+                </li>
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    className="get-started btn btn-outline-dark"
+                    onClick={() => {
+                      navigate("/read");
+                    }}
+                  >
+                    Read Blogs
+                  </button>
+                </li>
+              </ul>
+            ) : (
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link
+                    className="nav-link active"
+                    aria-current="page"
+                    to="/create"
+                  >
+                    Write
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">
+                    Profile
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/read">
+                    Read
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <button
+                    type="button"
+                    className="get-started btn btn-outline-dark"
+                    onClick={Logout}
+                  >
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </nav>
