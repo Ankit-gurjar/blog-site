061b19d8395b30776ed81ae59091712eb413f4dd
Ankit <agdjp56@gmail.com>
2024-04-01 19:56:32 +0530

    hook updated

--- server/index.js
+++ server/index.js
@@ -6,14 +6,12 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
 const blogRoute = require("./Routes/AuthRoute");
-const { MONGO_URL,PORT, CORS_URL } = process.env;
-
-
+const { MONGO_URL, PORT, CORS_URL } = process.env;
 
 mongoose
-.connect(MONGO_URL)
-.then(() => console.log("MongoDB is  connected successfully"))
-.catch((err) => console.error(err));
+  .connect(MONGO_URL)
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
@@ -21,16 +19,19 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: [CORS_URL, "http://localhost:3000", "http://localhost:5173" ],
+    origin: [
+      CORS_URL,
+      "https://blog-site-backend-fmo9.onrender.com",
+      "https://blog-site-y0gw.onrender.com/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-  );
-  app.use(cookieParser());
-  
-  app.use(express.json());
-  
-  app.use("/", authRoute);
-  app.use("/", blogRoute);
-  app.use('/uploads', express.static('uploads'));
+);
+app.use(cookieParser());
 
+app.use(express.json());
+
+app.use("/", authRoute);
+app.use("/", blogRoute);
+app.use("/uploads", express.static("uploads"));
