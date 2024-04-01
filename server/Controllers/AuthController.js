fc14b8f5d8758cdc97d0abbba519e8c85d13bfc4
Ankit <agdjp56@gmail.com>
2024-04-01 19:50:20 +0530

    hook updated

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -2,37 +2,38 @@ const User = require("../Models/UserModel");
 const { createSecretToken } = require("../util/SecretToken");
 const bcrypt = require("bcryptjs");
 const Post = require("../Models/PostModel");
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
+const expressAsyncHandler = require("express-async-handler");
 
-
-
-
-module.exports.Login = async (req, res) => {
+module.exports.Login = expressAsyncHandler(async (req, res) => {
   try {
     const { email, password } = req.body;
-    if(!email || !password){
-      return res.json({message:'All fields are required'});
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
     }
     const user = await User.findOne({ email });
-    if(!user){
-      return res.json({message:'Incorrect password or email'});
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
     }
-    const auth = await bcrypt.compare(password,user.password);
+    const auth = await bcrypt.compare(password, user.password);
     if (!auth) {
-      return res.json({message:'Incorrect password or email'});
+      return res.json({ message: "Incorrect password or email" });
     }
     const token = createSecretToken(user._id);
-    
-    
-    return res.status(201).json({ message: "User logged in successfully", success: true, token });
+
+    return res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true, token });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "An error occurred during the login process" });
+    return res
+      .status(500)
+      .json({ message: "An error occurred during the login process" });
   }
-};
+});
 
 // Signup Controller
-module.exports.Signup = async (req, res) => {
+module.exports.Signup = expressAsyncHandler(async (req, res) => {
   try {
     const { email, password, username, createdAt } = req.body;
     const existingUser = await User.findOne({ email });
@@ -41,85 +42,92 @@ module.exports.Signup = async (req, res) => {
     }
     const user = await User.create({ email, password, username, createdAt });
     const token = createSecretToken(user._id);
-    return res.status(201).json({ message: "User signed in successfully", success: true, user, token });
+    return res.status(201).json({
+      message: "User signed in successfully",
+      success: true,
+      user,
+      token,
+    });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ message: "An error occurred during the signup process" });
+    return res
+      .status(500)
+      .json({ message: "An error occurred during the signup process" });
   }
-};
-module.exports.createPost = async (req, res) => {
+});
+module.exports.createPost = expressAsyncHandler(async (req, res) => {
   try {
     const { postTitle, postBody } = req.body;
-    let image = '';
+    let image = "";
     if (req.file) {
-      image = req.file.path; 
+      image = req.file.path;
     }
-    const user = req.user; 
+    const user = req.user;
     const newPost = new Post({
       postTitle,
       postBody,
-      user: user._id, 
-      image: image
+      user: user._id,
+      image: image,
     });
 
     await newPost.save();
 
     await User.findByIdAndUpdate(
       user._id,
-      { $push: { posts: newPost._id } }, 
+      { $push: { posts: newPost._id } },
       { new: true, useFindAndModify: false }
     );
 
-
-    res.status(201).json({ message: "Post created successfully", post: newPost });
+    res
+      .status(201)
+      .json({ message: "Post created successfully", post: newPost });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error creating post" });
   }
-};
+});
 
-module.exports.getUserProfile = async (req, res) => {
+module.exports.getUserProfile = expressAsyncHandler(async (req, res) => {
   if (!req.user) {
     return res.status(401).json({ message: "Not authenticated" });
   }
-  
-  try {
 
+  try {
     const userWithPosts = await User.findById(req.user.id)
-    .populate({
-      path: 'posts', // Populate posts
-      populate: [
-        { path: 'comments.postedBy', select: 'username' }, // Populate comment creators
-        { path: 'likes', select: 'username' } // Populate users who liked the post
-      ]
-    })
-    .exec();
-    
+      .populate({
+        path: "posts", // Populate posts
+        populate: [
+          { path: "comments.postedBy", select: "username" }, // Populate comment creators
+          { path: "likes", select: "username" }, // Populate users who liked the post
+        ],
+      })
+      .exec();
+
     if (!userWithPosts) {
       return res.status(404).json({ message: "User not found" });
     }
-    
+
     res.json({ message: "User profile data", user: userWithPosts });
   } catch (error) {
     console.error("Failed to fetch user profile", error);
     res.status(500).json({ message: "Internal server error" });
   }
-};
+});
 
 // In your post controller
-module.exports.getPostById = async (req, res) => {
+module.exports.getPostById = expressAsyncHandler(async (req, res) => {
   try {
     const post = await Post.findById(req.params.postId);
     if (!post) {
-      return res.status(404).json({ message: 'Post not found' });
+      return res.status(404).json({ message: "Post not found" });
     }
     res.json({ post });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
-};
+});
 
-module.exports.likePost = async (req, res) => {
+module.exports.likePost = expressAsyncHandler(async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.user._id)) {
@@ -132,14 +140,14 @@ module.exports.likePost = async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-};
+});
 
-module.exports.addComment = async (req, res) => {
+module.exports.addComment = expressAsyncHandler(async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     const comment = {
       text: req.body.text,
-      user: req.user._id
+      user: req.user._id,
     };
     post.comments.push(comment);
     await post.save();
@@ -147,27 +155,26 @@ module.exports.addComment = async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-};
+});
 
-
-module.exports.getAllPosts = async (req, res) => {
+module.exports.getAllPosts = expressAsyncHandler(async (req, res) => {
   try {
     const posts = await Post.find()
-      .populate('user', 'username avatar') // Populate user details
-      .populate('comments.postedBy', 'username') // Populate comment creator details
+      .populate("user", "username avatar") // Populate user details
+      .populate("comments.postedBy", "username") // Populate comment creator details
       .sort({ createdAt: -1 }); // Sort by creation date, newest first
     res.json({ message: "All posts retrieved successfully", posts });
   } catch (error) {
     console.error("Failed to fetch posts", error);
     res.status(500).json({ message: "Internal server error" });
   }
-};
+});
 
-module.exports.updateAvatar = async (req, res) => {
+module.exports.updateAvatar = expressAsyncHandler(async (req, res) => {
   try {
     const user = await User.findById(req.user._id);
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     // Generate a new avatar using faker
@@ -177,6 +184,6 @@ module.exports.updateAvatar = async (req, res) => {
     res.json({ avatar: user.avatar });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Could not update avatar' });
+    res.status(500).json({ message: "Could not update avatar" });
   }
-};
+});
